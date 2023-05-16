85e06ddafc7a0b6e0bccc211e9845b9c31d0347b
soohyuneee <tlstngus95@naver.com>
2023-05-16 14:33:34 +0900

    feat: 추천검색어 get 요청 추가

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,33 +1,49 @@
 import apiRequest from "./index";
 
 const RESOURCE = "/todos";
+const SEARCH = "/search";
 
 export const getTodoList = async () => {
-  try {
-    const response = await apiRequest.get(`${RESOURCE}`);
+	try {
+		const response = await apiRequest.get(`${RESOURCE}`);
 
-    return response;
-  } catch (error) {
-    throw new Error("API getTodoList error");
-  }
+		return response;
+	} catch (error) {
+		throw new Error("API getTodoList error");
+	}
+};
+
+export const getTodoSearch = async (query, page) => {
+	try {
+		const response = await apiRequest.get(SEARCH, {
+			params: {
+				q: query,
+				page: page,
+				limit: 10,
+			},
+		});
+		return { resData: response.data };
+	} catch (error) {
+		throw new Error("API getTodoSearch error");
+	}
 };
 
 export const createTodo = async (data) => {
-  try {
-    const response = await apiRequest.post(`${RESOURCE}`, data);
+	try {
+		const response = await apiRequest.post(`${RESOURCE}`, data);
 
-    return response;
-  } catch (error) {
-    throw new Error("API createTodo error");
-  }
+		return response;
+	} catch (error) {
+		throw new Error("API createTodo error");
+	}
 };
 
 export const deleteTodo = async (id) => {
-  try {
-    const response = await apiRequest.delete(`${RESOURCE}/${id}`);
+	try {
+		const response = await apiRequest.delete(`${RESOURCE}/${id}`);
 
-    return response;
-  } catch (error) {
-    throw new Error("API deleteTodo error");
-  }
+		return response;
+	} catch (error) {
+		throw new Error("API deleteTodo error");
+	}
 };
