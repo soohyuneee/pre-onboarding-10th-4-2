22e27b8a4e27e925f2e9273c6cfce96371814536
soohyuneee <tlstngus95@naver.com>
2023-05-16 14:33:52 +0900

    feat: 드롭다운 컴포넌트 추가

---
+++ src/components/Dropdown.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import useCreateTodo from '../hooks/useCreateTodo';
+import useInfiniteScroll from '../hooks/useInfiniteScroll';
+
+const Dropdown = ({ dropdown, inputText, setInputText, setTodos }) => {
+	const { handleSubmit } = useCreateTodo();
+	const { listRef } = useInfiniteScroll();
+
+	const handleClick = (idx) => {
+		setInputText('');
+		handleSubmit(dropdown[idx], setTodos);
+	};
+
+	return dropdown.length ? (
+		<ul className="drop-list">
+			{dropdown.map((item, idx) => (
+				<React.Fragment key={idx}>
+					<li className="drop-item" onClick={() => handleClick(idx)}>
+						{item
+							.split(new RegExp(`(${inputText})`, 'gi'))
+							.map((text, index) =>
+								text.toLowerCase() === inputText.toLowerCase() ? (
+									<mark key={index}>{text}</mark>
+								) : (
+									<React.Fragment key={index}>{text}</React.Fragment>
+								)
+							)}
+					</li>
+					{idx === dropdown.length - 1 && <div ref={listRef}></div>}
+				</React.Fragment>
+			))}
+		</ul>
+	) : null;
+};
+
+export default Dropdown;
